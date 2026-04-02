daac6b7edea5709998f7f1c883e4a2e9ab010c39
attachment4 <karavashkinivan1995@gmail.com>
2026-04-02 11:43:36 -0800

    Add Tests BonusServicesTest

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -35,4 +35,78 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldCalculateForUnregisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    void shouldCalculateForUnregisteredAndOverLimit() {
+        BonusService service = new BonusService();
+        long amount = 100_000;
+        boolean registered = true;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndBoundaryUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 16_633;
+        boolean registered = true;
+        long expected = 498;
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndBoundaryExactLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 16_667;
+        boolean registered = true;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForUnregisteredAndBoundaryUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 49_999;
+        boolean registered = false;
+        long expected = 499;
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForUnregisteredAndBoundaryExactLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 50_000;
+        boolean registered = false;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
